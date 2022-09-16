fe1c9d06c2c70515c8215510114a3744e1cb91eb
B1910018 <tuyenb1910018@student.ctu.edu.vn>
2022-09-16 16:54:58 +0700

    Xay dung trang chi tiet san pham

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '/products/products_manager.dart';
+import '/products/product_detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple,).copyWith(
+          secondary: Colors.deepOrange,
+        ),
+      ),
+      home: SafeArea(
+        child: ProductDetailScreen(
+          ProductsManager().items[0],
+        ),
+      ),
+    );
+  }
+}
